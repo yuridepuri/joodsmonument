4f022ef23b651c28f3738dd296c0ec354bec319e
Yuri Vermeire <yuri.vermeire@jaarbeurs.nl>
2016-03-23 10:56:38 +0100

    second commit

---
+++ app/src/main/java/com/example/patrickweijs/myapplication/MapsActivity.java
@@ -0,0 +1,134 @@
+package com.example.patrickweijs.myapplication;
+
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends ActionBarActivity {
+
+    double latitude;
+    double longitude;
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        setTitle("Route");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        /* Toast.makeText(this, "Zet uw GPS aan zodat uw locatie gezien kan worden als de blauwe stip, zo kunt u naar het monument navigeren.", Toast.LENGTH_LONG).show();
+        */
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();{
+            alertDialog.setTitle("GPS");
+            alertDialog.setMessage("Zet uw GPS aan zodat uw locatie gezien kan worden als de blauwe stip, zo kunt u naar het monument navigeren.");
+            alertDialog.setButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // here you can add functions
+                }
+            });}
+
+        alertDialog.setIcon(R.drawable.icon);
+        alertDialog.show();
+
+        /* LatLng sydney = new LatLng(-33.867, 151.206); */
+        LatLng maliebaan = new LatLng(52.08793, 5.13114);
+
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(maliebaan, 9));
+
+        mMap.addMarker(new MarkerOptions()
+                .title("Station Maliebaan")
+                .snippet("Hier is de muur.")
+                .position(maliebaan));
+
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()) {
+            case R.id.lobbies:
+                finish();
+                startActivity(new Intent(getApplicationContext(), OverJoodsMonument.class));
+                return true;
+            case R.id.lobbycreation:
+                finish();
+                startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                return true;
+            case R.id.muur:
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/patrickweijs/myapplication/OverJoodsMonument.java
@@ -0,0 +1,93 @@
+package com.example.patrickweijs.myapplication;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class OverJoodsMonument extends ActionBarActivity {
+
+    Button b1, b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      //  ActionBar actionBar = getActionBar();
+//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.RED)); // set your desired color
+        setTitle("Over");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.over_joods_monument_layout);
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                toSite();
+            }
+        });
+        setTitle("Over");
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                toSiteSpoorwegmuseum();
+            }
+        });
+
+    }
+
+    public void toSite(){
+        Uri uri = Uri.parse("http://www.joodsmonumentutrecht.nl");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+    public void toSiteSpoorwegmuseum(){
+        Uri uri = Uri.parse("http://82.192.77.193/beladentreinen/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()) {
+            case R.id.lobbies:
+                finish();
+                startActivity(new Intent(getApplicationContext(), OverJoodsMonument.class));
+                return true;
+            case R.id.lobbycreation:
+                finish();
+                startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                return true;
+            case R.id.muur:
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
